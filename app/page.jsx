d18c246294c09a6f36908f455f20b15e0788fb5e
Nitee Naik <niteeshnaik77@gmail.com>
2026-02-20 16:16:06 +0530

    project init

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-self-auto items-center h-screen w-full ">
+      <h1>BeatCode</h1>
+      <Button>solve</Button>
+    </div>
+  );
+}
